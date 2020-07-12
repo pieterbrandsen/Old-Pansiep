cbbb7a1745605ce4d7e1ce80dbf9f862c31205e7
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-07-12 12:28:56 +0200

    CheckMissingMemory Function update

--- GameCode/main.js
+++ GameCode/main.js
@@ -460,55 +460,7 @@ module.exports.loop = function() {
           }
         }
 
-        function getOpenSpotsNearSource(source) {
-          const terrain = new Room.Terrain(roomName);
-          const sourcePos = source.pos;
-          let sourcePosX;
-          let sourcePosY;
-          let count = 8;
 
-          sourcePosX = source.pos.x-1;
-          sourcePosY = source.pos.y-1;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          sourcePosX = source.pos.x;
-          sourcePosY = source.pos.y-1;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          sourcePosX = source.pos.x+1;
-          sourcePosY = source.pos.y-1;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          sourcePosX = source.pos.x-1;
-          sourcePosY = source.pos.y;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          sourcePosX = source.pos.x+1;
-          sourcePosY = source.pos.y;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          sourcePosX = source.pos.x-1;
-          sourcePosY = source.pos.y+1;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          sourcePosX = source.pos.x;
-          sourcePosY = source.pos.y+1;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          sourcePosX = source.pos.x+1;
-          sourcePosY = source.pos.y+1;
-          if (terrain.get(sourcePosX,sourcePosY) == 1)
-          count--;
-
-          return count;
-        }
 
         function defendRoom() {
           if (flagMemory.enemyCount > 0) {
--- GameCode/module.checkMissingMemory.js
+++ GameCode/module.checkMissingMemory.js
@@ -2,6 +2,58 @@ module.exports = {
   run: function(roomName) {
     const room = Game.rooms[roomName];
     const flagMemory = Memory.flags[roomName];
+
+    function getOpenSpotsNearSource(source) {
+      const terrain = new Room.Terrain(roomName);
+      const sourcePos = source.pos;
+      let sourcePosX;
+      let sourcePosY;
+      let count = 8;
+
+      sourcePosX = source.pos.x-1;
+      sourcePosY = source.pos.y-1;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      sourcePosX = source.pos.x;
+      sourcePosY = source.pos.y-1;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      sourcePosX = source.pos.x+1;
+      sourcePosY = source.pos.y-1;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      sourcePosX = source.pos.x-1;
+      sourcePosY = source.pos.y;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      sourcePosX = source.pos.x+1;
+      sourcePosY = source.pos.y;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      sourcePosX = source.pos.x-1;
+      sourcePosY = source.pos.y+1;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      sourcePosX = source.pos.x;
+      sourcePosY = source.pos.y+1;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      sourcePosX = source.pos.x+1;
+      sourcePosY = source.pos.y+1;
+      if (terrain.get(sourcePosX,sourcePosY) == 1)
+      count--;
+
+      return count;
+    }
+
+
     if (!flagMemory.roomManager) {
       flagMemory.roomManager = {};
     }
