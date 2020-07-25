e6082f1519cf44961e6f5d720fd05c14c13ec87c
Pieter <pieterbrandsen2@gmail.com>
2020-07-25 23:22:25 +0200

    Towers Comments Added

--- mainModule.towers.js
+++ mainModule.towers.js
@@ -4,7 +4,9 @@ module.exports = {
     const flagMemory = Memory.flags[roomName];
     const towers = room.towers;
 
+    // If FlagMemory Has Data Of More Then One Enemy //
     if (flagMemory.enemyCount > 0) {
+      // Run Defend (Attack) For Each Tower //
       for (let tower of room.towers) {
         tower.defend();
       }
