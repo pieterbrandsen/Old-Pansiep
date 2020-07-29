031bd2fe441fae227bfc91d78e7901f687874efc
Pieter <pieterbrandsen2@gmail.com>
2020-07-29 16:35:25 +0200

    Attack Module Update, Comments + Rewrite

--- module.attack.js
+++ module.attack.js
@@ -1,93 +1,58 @@
 module.exports = {
   run: function(creep) {
-    const firstEnemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {filter: (creep) => {
-      return ((creep.getActiveBodyparts(ATTACK) > 0) || (creep.getActiveBodyparts(RANGED_ATTACK) > 0)) }
-    });
+    const firstEnemyId = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {filter: (creep) => {
+      return ((creep.getActiveBodyparts(ATTACK) > 0) || (creep.getActiveBodyparts(RANGED_ATTACK) > 0)).id
+    }});
 
-    if (firstEnemy) {
+    function attackCreep(targetId) {
+      // Get Target By Id //
+      const targetCreep = Game.getObjectById(targetId);
+
+      // Check if Creep Is Melee //
       if (creep.getActiveBodyparts(ATTACK) > 0) {
-        if (!creep.pos.inRangeTo(firstEnemy,1)) {
-          creep.travelTo(firstEnemy);
-        }
-        else {
-          creep.attack(firstEnemy);
-        }
+        // If Creep Is Not In Range Of TargetCreep, Travel To It //
+        if (!creep.pos.inRangeTo(targetCreep,1))
+        creep.travelTo(targetCreep);
+        // Else If In Range, Attack TargetCreep //
+        else
+        creep.attack(targetCreep);
 
-        if (!creep.pos.inRangeTo(firstEnemy,3)) {
-          creep.travelTo(firstEnemy);
-        }
-        else {
-          creep.rangedAttack(firstEnemy);
-        }
+        // If Creep Is Not In Range Of TargetCreep, Travel To It //
+        if (!creep.pos.inRangeTo(targetCreep,3))
+        creep.travelTo(targetCreep);
+        // Else If In Range, Attack TargetCreep //
+        else
+        creep.rangedAttack(targetCreep);
       }
-      else if (creep.getActiveBodyparts(RANGED_ATTACK) > 0) {
-        if (!creep.pos.inRangeTo(firstEnemy,3)) {
-          creep.travelTo(firstEnemy);
-        }
-        else {
-          creep.rangedAttack(firstEnemy);
-        }
+
+      // Check if Creep Is Ranged //
+      if (creep.getActiveBodyparts(RANGED_ATTACK) > 0) {
+        // If Creep Is Not In Range Of TargetCreep, Travel To It //
+        if (!creep.pos.inRangeTo(targetCreep,3))
+        creep.travelTo(targetCreep);
+        // Else If In Range, Attack TargetCreep //
+        else
+        creep.rangedAttack(targetCreep);
       }
     }
+
+    // Search For Enemy With Attack Or Ranged Attack Parts //
+    if (firstEnemyId !== undefined)
+    attackCreep(firstEnemyId);
     else {
-      const secondEnemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {filter: (creep) => {
-        return (creep.getActiveBodyparts(HEAL) > 0) }
-      });
-      if (secondEnemy) {
-        if (creep.getActiveBodyparts(ATTACK) > 0) {
-          if (!creep.pos.inRangeTo(secondEnemy,1)) {
-            creep.travelTo(secondEnemy);
-          }
-          else {
-            creep.attack(secondEnemy);
-          }
+      const secondEnemyId = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {filter: (creep) => {
+        return (creep.getActiveBodyparts(HEAL) > 0).id
+      }});
 
-          if (!creep.pos.inRangeTo(secondEnemy,3)) {
-            creep.travelTo(secondEnemy);
-          }
-          else {
-            creep.rangedAttack(firstEnemy);
-          }
-        }
-        else if (creep.getActiveBodyparts(RANGED_ATTACK) > 0) {
-          if (!creep.pos.inRangeTo(secondEnemy,3)) {
-            creep.travelTo(secondEnemy);
-          }
-          else {
-            creep.rangedAttack(secondEnemy);
-          }
-        }
-      }
+      if (secondEnemyId !== undefined)
+      attackCreep(secondEnemyId)
       else {
-        const thirdEnemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (thirdEnemy) {
-          if (creep.getActiveBodyparts(ATTACK) > 0) {
-            if (!creep.pos.inRangeTo(thirdEnemy,1)) {
-              creep.travelTo(thirdEnemy);
-            }
-            else {
-              creep.attack(thirdEnemy);
-            }
-
-            if (!creep.pos.inRangeTo(thirdEnemy,3)) {
-              creep.travelTo(thirdEnemy);
-            }
-            else {
-              creep.rangedAttack(thirdEnemy);
-            }
-          }
-          else if (creep.getActiveBodyparts(RANGED_ATTACK) > 0) {
-            if (!creep.pos.inRangeTo(thirdEnemy,3)) {
-              creep.travelTo(thirdEnemy);
-            }
-            else {
-              creep.rangedAttack(thirdEnemy);
-            }
-          }
+        const thirdEnemyId = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS).id;
+        if (thirdEnemyId)
+        attackCreep(thirdEnemyId)
+        else {
+          // Structure Finding Part //
         }
-        // else {
-        //   // Structures
-        // }
       }
     }
   }
