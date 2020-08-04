d8ade7fb80433e0d05899104c64d2e8580335c65
Pieter <pieterbrandsen2@gmail.com>
2020-08-04 20:40:14 +0200

    ToDoList update

--- ToDoList.js
+++ ToDoList.js
@@ -1,10 +1,24 @@
 /*
 
-
-terminal system
+1.
+remote harvesting, for small rooms and big rooms
+2.
 mineral harvesting
-
-Tracking of energy left in body's (this is loss)
+Attack Creep Spawn On Enemy Entry (thats big enough to tank towers or are more then 2 creeps)
+3.
+AttackModule Strucutres upgrade
+4.
+more tracking
+terminal system
+5.
+extensions and room spawns that need energy, put it in an array and get it from the transferer
+files recheck
+6.
+tower whitelist array
+7.
+8.
+9.
+10.
 
 
 */
