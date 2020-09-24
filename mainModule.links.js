4ab50119c87b4bd82a83b56d5f65baec23afcabc
Pieter <pieterbrandsen2@gmail.com>
2020-09-24 18:07:34 +0200

    Link Update, Now sends only what fits to the targetLink.

--- mainModule.links.js
+++ mainModule.links.js
@@ -3,90 +3,51 @@ module.exports = {
     const room = Game.rooms[roomName];
     const flagMemory = Memory.flags[roomName];
 
+
     function findLinkInRange(objectId,range) {
       // Check If Input Object Is Defined
       const object = Game.getObjectById(objectId);
-      let linkObject;
       if (object !== null) {
         // Check If Every Link In Memory Is Defined, Else Return Undefined //
         room.links.forEach((link, i) => {
-          if (!link)
-          return undefined
-          else if (link.pos.inRangeTo(object,range))
-          linkObject = link;
+          if (link) {
+            if (link.pos.inRangeTo(object,range))
+            return link.id;
+          }
         });
-
-        // If Link Is Found, Return The Id //
-        if (linkObject !== undefined)
-        return linkObject.id;
       }
-    }
+    };
 
     function transferLinkEnergy(fromLinkId,toLinkId) {
-      Game.getObjectById(fromLinkId).transferEnergy(Game.getObjectById(toLinkId))
-    }
+      const fromLink = Game.getObjectById(fromLinkId);
+      const toLink = Game.getObjectById(toLinkId);
+      let transferEnergyAmount = 0;
 
-    // If Link Has Its Memory //
-    if (flagMemory.links) {
-      // If Both Links Has Memory //
-      if (flagMemory.links.linkTo1 && flagMemory.links.linkTo2) {
-        // Try To Transfer Energy From One To The Other //
-        if (!Game.getObjectById(flagMemory.links.linkTo1))
-        flagMemory.links.linkTo1 = "";
-        if (!Game.getObjectById(flagMemory.links.linkTo2))
-        flagMemory.links.linkTo2 = "";
 
-        transferLinkEnergy(flagMemory.links.linkTo1, flagMemory.links.linkTo2);
+      if (toLink && fromLink) {
+        if (toLink.store.getFreeCapacity(RESOURCE_ENERGY) <= fromLink.store.getUsedCapacity(RESOURCE_ENERGY))
+        transferEnergyAmount = toLink.store.getFreeCapacity(RESOURCE_ENERGY);
+        else
+        transferEnergyAmount = fromLink.store.getUsedCapacity(RESOURCE_ENERGY);
+
+
+        if (transferEnergyAmount > 0)
+        fromLink.transferEnergy(toLink, transferEnergyAmount);
       }
+    };
 
-      // If Both Links Has Memory //
-      if (flagMemory.links.linkFrom1 && flagMemory.links.linkTo1) {
-        // Try To Transfer Energy From One To The Other //
-        if (!Game.getObjectById(flagMemory.links.linkFrom1))
-        flagMemory.links.linkFrom1 = "";
-        if (!Game.getObjectById(flagMemory.links.linkTo1))
-        flagMemory.links.linkTo1 = "";
+    function resetLinks() {
+      if (!Game.getObjectById(flagMemory.links.linkFrom1))
+      flagMemory.links.linkFrom1 = "";
+      if (!Game.getObjectById(flagMemory.links.linkFrom2))
+      flagMemory.links.linkFrom2 = "";
+      if (!Game.getObjectById(flagMemory.links.linkTo1))
+      flagMemory.links.linkTo1 = "";
+      if (!Game.getObjectById(flagMemory.links.linkTo2))
+      flagMemory.links.linkTo2 = "";
+    };
 
-        transferLinkEnergy(flagMemory.links.linkFrom1, flagMemory.links.linkTo1);
-      }
-
-      // If Both Links Has Memory //
-      if (flagMemory.links.linkFrom2 && flagMemory.links.linkTo1) {
-        // Try To Transfer Energy From One To The Other //
-        if (!Game.getObjectById(flagMemory.links.linkFrom2))
-        flagMemory.links.linkFrom2 = "";
-        if (!Game.getObjectById(flagMemory.links.linkTo1))
-        flagMemory.links.linkTo1 = "";
-
-        transferLinkEnergy(flagMemory.links.linkFrom2, flagMemory.links.linkTo1);
-      }
-
-
-      // If Room Is Missing This Link //
-      if (!flagMemory.links.linkTo1) {
-        if (room.controller.level >= 6) {
-          const headSpawn = Game.getObjectById(flagMemory.roomManager.headSpawn);
-          if (headSpawn) {
-            const link = findLinkInRange(headSpawn.id,3);
-
-            // If Link Is Found, Save The Link //
-            if (link)
-            flagMemory.links.linkTo1 = link;
-          }
-        }
-      }
-
-      // If Room Is Missing This Link //
-      if (!flagMemory.links.linkTo2) {
-        if (room.controller.level >= 6) {
-          const link = findLinkInRange(room.controller.id,3);
-
-          // If Link Is Found, Save The Link //
-          if (link)
-          flagMemory.links.linkTo2 = link;
-        }
-      }
-
+    function getAllLinks() {
       // If Room Is Missing This Link //
       if (!flagMemory.links.linkFrom1) {
         if (room.controller.level >= 6) {
@@ -97,10 +58,9 @@ module.exports = {
           flagMemory.links.linkFrom1 = link;
         }
       }
-
       // If Room Is Missing This Link //
       if (!flagMemory.links.linkFrom2) {
-        if (room.controller.level >= 7) {
+        if (room.controller.level >= 7 && flagMemory.sources.length > 1) {
           // Search Link //
           const link = findLinkInRange(flagMemory.sources[1].id,3);
 
@@ -109,6 +69,34 @@ module.exports = {
           flagMemory.links.linkFrom2 = link;
         }
       }
+      // If Room Is Missing This Link //
+      if (!flagMemory.links.linkTo1) {
+        const link = findLinkInRange(flagMemory.roomManager.headSpawn,3);
+
+        // If Link Is Found, Save The Link //
+        if (link)
+        flagMemory.links.linkTo1 = link;
+      }
+      // If Room Is Missing This Link //
+      if (!flagMemory.links.linkTo2) {
+        const link = findLinkInRange(room.controller.id,3);
+
+        // If Link Is Found, Save The Link //
+        if (link)
+        flagMemory.links.linkTo2 = link;
+      }
+    };
+
+    // If Link Has Its Memory //
+    if (flagMemory.links && flagMemory.controllerLevel) {
+      if (Game.time % 100 == 0) {
+        resetLinks();
+        getAllLinks();
+      }
+
+      transferLinkEnergy(flagMemory.links.linkTo1, flagMemory.links.linkTo2);
+      transferLinkEnergy(flagMemory.links.linkFrom1, flagMemory.links.linkTo1);
+      transferLinkEnergy(flagMemory.links.linkFrom2, flagMemory.links.linkTo1);
     }
   }
 }
