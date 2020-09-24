853e2d9b62c2c7b7b0d06741a643957b71d73fc9
Pieter <pieterbrandsen2@gmail.com>
2020-09-24 18:09:02 +0200

    ToDoList Update

--- ToDoList.js
+++ ToDoList.js
@@ -1,26 +1,22 @@
 /*
 
-ramparts
-multi part boosting
 1.
 2.
 Roads in remote auto.
 Build roads everywhere
 Automate remoting
-let creeps check if vision in remote room, not walk into it.
 3.
 Factory code
 Power code
 Power Harvesting Code
 Deposit Harvesting Code
 4.
-Ghodium is not being inputted in Memory at the right time, make your own reaction list.
 Market code
 5.
 6.
 Advanced Market code
 7.
-Advanced advancd market code
+Super advancd market code
 8.
 9.
 10.
