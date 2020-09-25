35e1f5fda63bc43d0544dbb7f15294b5d57dfdfc
pieterbrandsen <45623682+pieterbrandsen@users.noreply.github.com>
2020-09-25 12:33:24 +0200

    Claimer was not moving to target room, fixed

--- creepModule.claim.js
+++ creepModule.claim.js
@@ -1,39 +1,45 @@
 module.exports = {
   run: function(creep) {
     function claimRoom() {
-      const target = creep.room.controller;
-      const runClaimController = creep.claimController(target);
+      const targetRoom = Game.rooms[creep.memory.targetRoom];
 
-      switch(runClaimController) {
-        case OK:
-        creep.say("Claimed");
-        creep.room.createFlag(25,25,"builderLD"+creep.memory.spawnRoom);
-        Game.flags["claim"].remove();
+      if (targetRoom) {
+        const target = targetRoom.controller;
+        const runClaimController = creep.claimController(target);
+
+        switch(runClaimController) {
+          case OK:
+          creep.say("Claimed");
+          creep.room.createFlag(25,25,"builderLD"+creep.memory.spawnRoom);
+          Game.flags["claim"].remove();
           break;
-        case ERR_NOT_OWNER:
+          case ERR_NOT_OWNER:
           break;
-        case ERR_BUSY:
+          case ERR_BUSY:
           break;
-        case ERR_INVALID_TARGET:
+          case ERR_INVALID_TARGET:
           if (creep.room.controller.owner !== 'PandaMaster') {
             creep.attackController(creep.room.controller);
             creep.travelTo(target);
           }
           break;
-        case ERR_FULL:
+          case ERR_FULL:
           break;
-        case ERR_NOT_IN_RANGE:
+          case ERR_NOT_IN_RANGE:
           creep.say("Moving");
           creep.travelTo(target);
           break;
-        case ERR_NO_BODYPART:
+          case ERR_NO_BODYPART:
           break;
-        case ERR_GCL_NOT_ENOUGH:
-        Game.notify("GCL IS NOT HIGH ENOUGH FOR CLAIMING");
+          case ERR_GCL_NOT_ENOUGH:
+          Game.notify("GCL IS NOT HIGH ENOUGH FOR CLAIMING");
           break;
-        default:
+          default:
           break;
+        }
       }
+      else
+      creep.travelTo(Game.flags[creep.memory.targetRoom]);
     }
 
     // Run Claimer Module //
