eef7039724e1aa757c624a34a32259c834a216b6
Pieter <pieterbrandsen2@gmail.com>
2020-11-13 10:32:10 +0100

    Updated main file

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
+//#region Require('./)
 import { ErrorMapper } from "utils/ErrorMapper";
-
+import { MainHandler } from "./handlers/mainHandler";
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
-export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+//#endregion
 
-  // Automatically delete memory of missing creeps
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
-    }
-  }
+//#region Game loop
+export const loop = ErrorMapper.wrapLoop(() => {
+  // Run main handler
+  // This will call the rest of the handlers
+  MainHandler();
 });
+//#endregion
