9f9fe5069336a9566f6b6467453bb552f4c89176
Pieter <pieterbrandsen2@gmail.com>
2020-11-13 17:10:47 +0100

    Updated many types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,22 +1,59 @@
-// example declaration file - remove these and add your own custom typings
+interface MyRoom {
+  spawns: Array;
+}
 
-// memory extension samples
 interface CreepMemory {
   role: string;
   room: string;
   working: boolean;
 }
 
-interface Memory {
-  stats: stats;
-  uuid: number;
-  log: any;
+interface FlagMemory {
+  commonMemory: {
+    sourceCount: number;
+    mineral: { id: string | undefined; type: string | undefined; amount: number | undefined };
+    sources: { id: string; pos: RoomPosition } | emptyArray;
+    constructionSites: [id: string] | emptyArray;
+    energyStructures: [id: string] | emptyArray;
+    repair: { targets: Array[string]; hitsTarget: number };
+    controllerLevel?: number;
+    headSpawnId?: string | undefined;
+    spawnEnergyStructures?: Array[string] | emptyArray;
+    energyStored: { usable: number; capacity: number };
+    controllerStorage?: { usable: number };
+    links?: {
+      source0: string | undefined;
+      source1: string | undefined;
+      controller: string | undefined;
+      head: string | undefined;
+    };
+  };
+  roomPlanner: { room: { sources: Object[] }; base?: Object };
+  enemies: { parts: CREEP_PART; creeps: Array[string] };
+  damagedCreeps: Array[string] | emptyArray;
+  remotes: { totalSourceCount: number; rooms: Array[string] | EmptyArray };
+  isFilled: boolean;
 }
 
-interface stats {
+interface Memory {
+  stats: stats;
+  isFilled: boolean;
+}
+
+interface Stats {
   ticksAlive: number;
-  uuid: number;
-  log: any;
+  gcl: object;
+  rooms: {
+    [key: string]: object;
+    energyStored: object;
+    commonMemory: object;
+    performance: { expenses: object; income: object };
+    energyStored: object;
+    spawnerEnergy: object;
+    controller: object;
+  };
+  common: object;
+  cpu: { headModules: object; smallModules: object };
 }
 
 // `global` extension samples
