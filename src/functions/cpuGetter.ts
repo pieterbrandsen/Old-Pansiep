f047c1a9840066ddf493c5fafcd02944b1471219
Pieter <pieterbrandsen2@gmail.com>
2020-11-13 11:29:28 +0100

    Added function
    Cpu getter will get the currently used cpu without duplicating code

---
+++ src/functions/cpuGetter.ts
@@ -0,0 +1,10 @@
+// Get currently used cpu amount
+const cpuGetter = ():number => Game.cpu.getUsed();
+
+// pre and end cpu getter is for later, when you want to log pre and end cpu.
+const preCpuGetter = ():number => cpuGetter();	
+const endCpuGetter = ():number => cpuGetter();	
+
+//#region Export functions
+export {preCpuGetter as PreCpuGetter, endCpuGetter as EndCpuGetter};
+//#endregion
