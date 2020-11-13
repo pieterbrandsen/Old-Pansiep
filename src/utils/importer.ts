93aea5db6ce200c26336323ad8dfb46c170f8d4c
Pieter <pieterbrandsen2@gmail.com>
2020-11-13 18:09:41 +0100

    Moved importer file to a new location

--- src/utils/importer.ts
+++ src/utils/importer.ts
@@ -1,12 +1,19 @@
 //#region Require('./)
-export * from "./functions/cpuGetter";
-export * from "./functions/isMemoryPathDefined";
-export * from "./functions/loadMemory";
+// * Functions //
+export * from "../functions/cpuGetter";
+export * from "../functions/isMemoryPathDefined";
+export * from "../functions/loadMemory";
+
+// * Handlers
 // import * as CreepHandlerFile from "./handlers/creep";
-// import * as GlobalHandlerFile from "./handlers/global";
-export * from "./handlers/memory";
+export * from "../handlers/global";
+export * from "../handlers/memory";
 // import * as OwnedRoomHandlerFile from "./handlers/ownedRoom";
 // import * as RemoteRoomHandlerFile from "./handlers/remoteRoom";
 // import * as RoomHandlerFile from "./handlers/room";
 // import * as CreepHandlerFile from "./handlers/stats";
+
+// * Utils
+export * from "./config";
+export * from "./ErrorMapper";
 //#endregion
