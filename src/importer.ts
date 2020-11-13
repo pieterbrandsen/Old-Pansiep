97eb21483a7a80fbd5923d1efef0399803153b0b
Pieter <pieterbrandsen2@gmail.com>
2020-11-13 17:10:32 +0100

    Added a importer to import other files from for files that need it

---
+++ src/importer.ts
@@ -0,0 +1,12 @@
+//#region Require('./)
+export * from "./functions/cpuGetter";
+export * from "./functions/isMemoryPathDefined";
+export * from "./functions/loadMemory";
+// import * as CreepHandlerFile from "./handlers/creep";
+// import * as GlobalHandlerFile from "./handlers/global";
+export * from "./handlers/memory";
+// import * as OwnedRoomHandlerFile from "./handlers/ownedRoom";
+// import * as RemoteRoomHandlerFile from "./handlers/remoteRoom";
+// import * as RoomHandlerFile from "./handlers/room";
+// import * as CreepHandlerFile from "./handlers/stats";
+//#endregion
