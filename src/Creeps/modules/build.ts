54417b6a58f602b73d9accb08d4b80505014d01a
Pieter <pieterbrandsen2@gmail.com>
2020-11-14 22:59:53 +0100

    Added build module

---
+++ src/Creeps/modules/build.ts
@@ -0,0 +1,91 @@
+//#region Require('./)
+import { Config } from "../../Utils/importer";
+//#endregion
+
+//#region Functions()
+const build = (creep: Creep) => {
+  // Make shortcut to memory
+  const creepMemory: CreepMemory = creep.memory;
+  const flagMemory: FlagMemory = Memory.flags[creepMemory.targetRoom];
+
+  // Return empty if current creep's storage is empty
+  if (creep.store.getUsedCapacity() === 0) return "empty";
+
+  // If there are no construction sites left and no target, return full to get another goal if possible
+  if (flagMemory.commonMemory.constructionSites.length === 0 && !creepMemory.targetId) {
+    if (creep.room.controller && !creep.pos.inRangeTo(creep.room.controller, 5)) creep.moveTo(creep.room.controller);
+    return "full";
+  }
+
+  // If creep is missing a targetId
+  if (creepMemory.targetId === undefined) {
+    // Get the fist target from what is saved //
+    // This target will get later shifted when its completed
+    creep.memory.targetId = flagMemory.commonMemory.constructionSites[0];
+  } else {
+    // Get the saved construction site from memory
+    const constructionSite: ConstructionSite | null = Game.getObjectById(creepMemory.targetId);
+
+    // If construction site doesn't exist, remove it
+    if (constructionSite === null) return flagMemory.commonMemory.constructionSites.shift();
+
+    // Run the build function
+    const result = creep.build(constructionSite);
+    // Switch based on the results
+    switch (result) {
+      case OK:
+        Config.expenses.building[creep.room.name] += creep.memory.parts.work * 5;
+        break;
+      case ERR_INVALID_TARGET:
+        // TODO SAVE THE TARGET POS BECAUSE ATM THE CONSTRUCTION IS ALREADY LOST
+        // // If the structure is a rampart, heal it until 5K hits
+        // if (
+        //   constructionSite &&
+        //     constructionSite.structureType === STRUCTURE_RAMPART
+        // ) {
+        //   // Find all structures at last known position
+        //   const foundStructures = creep.room.lookForAt(
+        //     LOOK_STRUCTURES,
+        //     constructionSite.pos.x,
+        //     constructionSite.pos.y,
+        //   );
+
+        //   // Loop through all structures that are found at last know position and try to find a rampart
+        //   let repairStructureId;
+        //   foundStructures.forEach((structure) => {
+        //     if (structure.structureType === STRUCTURE_RAMPART) {
+        //       repairStructureId = structure.id;
+        //     }
+        //   });
+
+        //   // If a rampart was found, repair that structure
+        //   if (repairStructureId) {
+        //     repair({id: repairStructureId});
+        //     const repairStructure = Game.getObjectById(repairStructureId);
+        //     if (repairStructure.hits < 5000) {
+        //       return;
+        //     }
+        //   }
+        // }
+
+        // Check if target that's going to be lost is still on the construction list, if so shift it.
+        if (flagMemory.commonMemory.constructionSites.indexOf(creep.memory.targetId) === 0) {
+          flagMemory.commonMemory.constructionSites.shift();
+        }
+        // Delete target from the memory
+        delete creep.memory.targetId;
+        return "full";
+      case ERR_NOT_IN_RANGE:
+        // If creep is not in range, move to target
+        creep.moveTo(constructionSite);
+        return;
+      default:
+        break;
+    }
+  }
+};
+//#endregion
+
+//#region Export functions
+export { build as Build };
+//#endregion
