17fa7af5cc78216de933c5945978f66c019b7348
Pieter <pieterbrandsen2@gmail.com>
2020-11-15 12:27:10 +0100

    Added function for room handlers

---
+++ src/functions/getRandomFreePos.ts
@@ -0,0 +1,23 @@
+//#region Functions()
+const getRandomFreePos = (startPos:{x: number, y: number, roomName: string}):RoomPosition => {
+  // Get the terrain of the Room //
+  const terrain:RoomTerrain = Game.map.getRoomTerrain(startPos.roomName);
+  const distance: number = 0;
+  let x:number;
+  let y:number;
+
+  // Loop until a random non-wall position is found
+  do {
+    x = startPos.x + Math.floor(Math.random() * (distance * 2 + 1)) - distance;
+    y = startPos.y + Math.floor(Math.random() * (distance * 2 + 1)) - distance;
+  } while (
+    (x + y) % 2 !== (startPos.x + startPos.y) % 2 ||
+    terrain.get(x, y) === TERRAIN_MASK_WALL
+  );
+  return new RoomPosition(x, y, startPos.roomName);
+}
+//#endregion
+
+//#region Export functions
+export {getRandomFreePos as GetRandomFreePos};
+//#endregion
