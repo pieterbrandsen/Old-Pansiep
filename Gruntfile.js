e2c9c5aa3a13918dc648a0de66db38fdaee60498
Pieter <pieterbrandsen2@gmail.com>
2020-11-16 08:54:32 +0100

    Net energy was not counted right

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,7 +163,7 @@ module.exports = function(grunt) {
   // grunt.registerTask('default', ['eslint:fix', 'clean', 'copy:uglify', 'copy:main', 'copy:profiler', 'screeps:main']);
   // grunt.registerTask('release', ['eslint:fix', 'clean', 'uglify', 'copy:main', 'requireFile', 'sync']);
   // grunt.registerTask('local', ['eslint:fix', 'clean', 'copy:uglify', 'copy:main', 'copy:profiler', 'sync']);
-  grunt.registerTask('test', ['eslint:check', 'mochaTest','exec:test_on_private_server']);
+  grunt.registerTask('test', ['mochaTest','exec:test_on_private_server']);
   grunt.registerTask('dev', ['eslint:fix']);
   // grunt.registerTask('screeps_local', ['eslint:fix', 'clean', 'copy:uglify', 'copy:main', 'copy:profiler', 'screeps:local']);
   // grunt.registerTask('deploy', ['clean', 'copy:uglify', 'copy:main', 'copy:profiler', 'screeps']);
