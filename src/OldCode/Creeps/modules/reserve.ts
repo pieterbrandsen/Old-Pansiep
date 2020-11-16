446a399fbbb3a8abab814ce9bafdb587caeed52b
Pieter <pieterbrandsen2@gmail.com>
2020-11-16 19:57:34 +0100

    Added Old code as folder, REMOVE LATER

---
+++ src/OldCode/Creeps/modules/reserve.ts
@@ -0,0 +1,19 @@
+//#region Functions()
+const reserve = (creep: Creep) => {
+  if (creep.room.controller === undefined) return;
+  const result = creep.reserveController(creep.room.controller);
+  // Switch based on the results
+  switch (result) {
+    case ERR_NOT_IN_RANGE:
+      // If creep is not in range, move to target
+      creep.moveTo(creep.room.controller);
+      break;
+    default:
+      break;
+  }
+};
+//#endregion
+
+//#region Export functions
+export { reserve as Reserve };
+//#endregion
---
+++ src/OldCode/functions/loadMemory.ts
@@ -0,0 +1,13 @@
+//#region Require('./)
+import { IsMemoryPathDefined } from "./isMemoryPathDefined";
+//#endregion
+
+//#region Functions()
+const memoryLoader = (): void => {
+  Memory;
+};
+//#endregion
+
+//#region Export functions
+export { memoryLoader as MemoryLoader };
+//#endregion
