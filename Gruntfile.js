b205b25ecb03ef2c6d513bb2abf041e8ba09219f
Pieter <pieterbrandsen2@gmail.com>
2020-11-17 10:58:49 +0100

    Added grunt command to run typedoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,20 @@ try {
 }
 
 module.exports = function(grunt) {
-    grunt.loadNpmTasks("grunt-jsdoc");
-
+    grunt.loadNpmTasks("grunt-typedoc");
     grunt.initConfig({
+        typedoc: {
+            build: {
+                options: {
+                    module: "esnext",
+                    target: "es2017",
+                    out: "docs/",
+                },
+                src: "src/**/*"
+            }
+        }
     });
 
     grunt.log.writeln(new Date().toString());
+    grunt.registerTask("run-typedoc", ["typedoc"]);
 };
