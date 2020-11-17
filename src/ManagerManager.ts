da19ea64330130ddb6ea8e54f132d62a3e7a3011
Pieter <pieterbrandsen2@gmail.com>
2020-11-17 11:07:30 +0100

    Memory manager added to ManagerManager

--- src/ManagerManager.ts
+++ src/ManagerManager.ts
@@ -1,5 +1,5 @@
 //#region Require('./)
-import { RoomManager } from "Utils/importer/internals";
+import { RoomManager, MemoryManager } from "Utils/importer/internals";
 //#endregion
 
 //#region Class
@@ -8,6 +8,17 @@ export class ManagerManager {
      * Run all managers that need to be called by the ManagerManager
      */
     public static runManagerManager(): void {
+        try {
+            // Run the memoryManager
+            // This will run all memory related management
+            // Like setting up the memory
+            MemoryManager.runMemoryManager();
+        } catch (err) {
+            console.log("There was an error in the runMemoryManager function");
+            console.log("File: Src/ManagerManager.ts");
+            console.log(`Error: ${err}`);
+        }
+
         try {
             // Run the roomManager
             // This will run all room related management
