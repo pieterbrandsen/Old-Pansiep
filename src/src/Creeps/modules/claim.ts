fc2462332e30b682d2d8f48393078a43e77fe4ef
Pieter <pieterbrandsen2@gmail.com>
2020-11-19 18:20:29 +0100

    Old files added to reuse them

---
+++ src/src/Creeps/modules/claim.ts
@@ -0,0 +1,41 @@
+//#region Functions()
+const claim = (creep: Creep) => {
+  // Make shortcut to memory
+  const creepMemory: CreepMemory = creep.memory;
+
+  // If there is no controller in target room
+  if (creep.room.controller === undefined) return;
+
+  // Run claim controller
+  const result = creep.claimController(creep.room.controller);
+
+  // Switch based on the results
+  switch (result) {
+    case OK:
+      // Delete flag and memory
+      Game.flags["claim"].remove();
+      delete Memory.flags["claim"];
+
+      // Create a builderLD flag and give it memory
+      // @ts-ignore
+      creep.room.createFlag(creep.pos, `builderLD${creepMemory.spawnRoom}`);
+      //@ts-ignore
+      Memory.flags[`builderLD${creepMemory.spawnRoom}`] = {
+        // @ts-ignore
+        spawnRoom: creepMemory.spawnRoom,
+        room: creep.room.name
+      };
+      break;
+    case ERR_NOT_IN_RANGE:
+      // If creep is not in range, move to target
+      creep.moveTo(creep.room.controller);
+      return;
+    default:
+      break;
+  }
+};
+//#endregion
+
+//#region Export functions
+export { claim as Claim };
+//#endregion
---
+++ src/src/handlers/memory.ts
@@ -0,0 +1,155 @@
+//#region Require('./)
+import { OwnedRoomTimers, RemoteRoomTimers } from "../Utils/importer/internals";
+//#endregion
+
+//#region Functions()
+const globalMemory = (): void => {
+  // Set all memory paths to empty objects
+  Memory.creeps = {};
+  Memory.flags = {};
+  Memory.stats = {
+    ticksAlive: 0,
+    gcl: {},
+    rooms: {},
+    common: {},
+    cpu: { headModules: {}, smallModules: {} }
+  };
+
+  // If there are 4 keys (all paths), set the memory to filled
+  if (Object.keys(Memory).length === 3) Memory.isFilled = true;
+};
+
+const globalRoomMemory = (room: Room): void => {
+  // Acces the flagMemory of this room
+  const flagMemory: FlagMemory = Memory.flags[room.name];
+
+  // Set the sources array to a empty array
+  const sources: FlagMemory["commonMemory"]["sources"][] = [];
+
+  // Loop through each source and push the id and pos to the empty source array
+  room.find(FIND_SOURCES).forEach(source => {
+    sources.push({ id: source.id, pos: source.pos });
+  });
+
+  // Reset this room in the stats memory
+  Memory.stats.rooms[room.name] = {
+    energyStored: {},
+    commonMemory: {},
+    performance: { expenses: {}, income: {} },
+    cpu: { headModules: { creeps: {} }, smallModules: {}, creepModules: {} }
+  };
+
+  // Set all commonMemory for a owned and remote room
+  flagMemory.commonMemory = {
+    // Set the source length
+    sourceCount: sources.length,
+    // Get the id, type and amount from the mineral in this room
+    mineral: {
+      id: room.find(FIND_MINERALS)[0] ? room.find(FIND_MINERALS)[0].id : "",
+      type: room.find(FIND_MINERALS)[0] ? room.find(FIND_MINERALS)[0].mineralType : "",
+      amount: room.find(FIND_MINERALS)[0] ? Math.round(room.find(FIND_MINERALS)[0].mineralAmount) : 0
+    },
+    // Set the id and pos of all sources to the memory of the room
+    sources: sources,
+    // Create a empty array for storing constructionSites
+    constructionSites: [],
+    // Create a empty array for storing energyStructures
+    energyStructures: [],
+    // Set the repair object
+    // Create a empty array for storing targets to repair
+    // Set the hitsTarget to a default of 250K
+    repair: {
+      targets: [],
+      hitsTarget: 250 * 1000
+    },
+    // Set the energyStored to a 0 for both of the keys
+    energyStored: { usable: 0, capacity: 0 }
+  };
+
+  // Set the roomPlanner object to the template
+  flagMemory.roomPlanner = { room: { sources: [] } };
+
+  // Set the enemies object to the template
+  flagMemory.enemies = {
+    parts: { ATTACK: 0, RANGED_ATTACK: 0, TOUGH: 0, HEAL: 0 },
+    creeps: []
+  };
+
+  // Sets the damagedCreeps object to a empty array
+  flagMemory.damagedCreeps = [];
+};
+
+const ownedRoomMemory = (room: Room & MyRoom): void => {
+  // Run all the memory getters from the globalRoom part
+  globalRoomMemory(room);
+
+  // Acces the flagMemory of this room
+  const flagMemory: FlagMemory = Memory.flags[room.name];
+
+  // Set the spawnerEnergy key to a empty object
+  Memory.stats.rooms[room.name].spawnerEnergy = {};
+  // Set the controller key to a empty object
+  Memory.stats.rooms[room.name].controller = {};
+
+  // Set the roomPlanner base key to a empty object
+  flagMemory.roomPlanner.base = {};
+
+  // Set the controller level to current controller level
+  flagMemory.commonMemory.controllerLevel = room.controller ? room.controller.level : undefined;
+
+  // Set and get the headSpawnId
+  flagMemory.commonMemory.headSpawnId = room.terminal
+    ? room.terminal.pos.findInRange(room.spawns, 2)[0]
+      ? // @ts-ignore: Id DOES exist on the result
+        room.terminal.pos.findInRange(room.spawns, 2)[0].id
+      : room.spawns[0].id
+    : room.spawns[0]
+    ? room.spawns[0].id
+    : room.find(FIND_STRUCTURES, {
+        filter: s => s.structureType === STRUCTURE_SPAWN
+      }).length > 0
+    ? room.find(FIND_STRUCTURES, {
+        filter: s => s.structureType === STRUCTURE_SPAWN
+      })[0].id
+    : undefined;
+  // Create a empty array for storing spawnEnergyStructures
+  flagMemory.commonMemory.spawnEnergyStructures = [];
+  // Set the storage in the controller storage to 0
+  flagMemory.commonMemory.controllerStorage = { usable: 0, type: undefined, id: "" };
+  // Set all links to undefined
+  flagMemory.commonMemory.links = {
+    source0: "",
+    source1: "",
+    head: "",
+    controller: ""
+  };
+  // Set the remotes object to default template
+  // flagMemory.remotes = { totalSourceCount: 0, rooms: [] };
+
+  // Run all timers for this room
+  OwnedRoomTimers(room);
+
+  // Check if Game.time is dividable by 10, if so set the isFilled to true
+  // By the time this is true it should be filled
+  if (Game.time % 10 === 0) flagMemory.isFilled = true;
+};
+
+const remoteRoomMemory = (room: Room & MyRoom): void => {
+  // Run all the memory getters from the globalRoom part
+  globalRoomMemory(room);
+
+  // Acces the flagMemory of this room
+  const flagMemory: FlagMemory = Memory.flags[room.name];
+
+  // Run all timers for this room
+  RemoteRoomTimers(room);
+
+  // Check if Game.time is dividable by 10, if so set the isFilled to true
+  // By the time this is true it should be filled
+  if (Game.time % 10 === 0) flagMemory.isFilled = true;
+};
+//#endregion
+
+//#region Export functions
+export { globalMemory as GlobalMemory, ownedRoomMemory as OwnedRoomMemory, remoteRoomMemory as RemoteRoomMemory };
+//#endregion
