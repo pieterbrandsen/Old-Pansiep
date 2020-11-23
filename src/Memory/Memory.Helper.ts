72c2916bd443ebf7783cd9331aa21bffb4f20c41
Pieter <pieterbrandsen2@gmail.com>
2020-11-23 21:06:07 +0100

    Added MemoryHelper this will help with all memory related things

---
+++ src/Memory/Memory.Helper.ts
@@ -0,0 +1,24 @@
+//#region Require('./)
+import _ from "lodash";
+import {} from "Utils/importer/internals";
+//#endregion
+
+//#region Class
+export class MemoryHelper {
+  public static getOnlyObjectsFromIDs<T>(idArray: string[]): T[] {
+    if (idArray.length === 0) {
+        return [];
+    }
+
+    const objects: T[] = [];
+    _.forEach(idArray, (id: string) => {
+        const object: T | null = Game.getObjectById(id);
+        if (object !== null) {
+            objects.push(object);
+        }
+    });
+
+    return objects;
+}
+}
+//#endregion
